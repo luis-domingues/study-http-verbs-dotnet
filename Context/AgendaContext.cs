afa80bb23abff3d1f3fc70dc1804279cae5821fe
luis-domingues <luisdomingues117@gmail.com>
2024-09-17 20:20:19 -0300

    feat: add new database context

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCrud.Entities;
+
+namespace WebApiCrud.Context;
+
+public class AgendaContext : DbContext
+{
+    public AgendaContext(DbContextOptions<AgendaContext> options) : base(options) {  }
+    
+    public DbSet<Contato> Contatos { get; set; }
+}
