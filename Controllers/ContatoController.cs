2b61c7f05421ee46a0546683d4534c3220e224af
luis-domingues <luisdomingues117@gmail.com>
2024-09-17 20:19:19 -0300

    feat: create endpoint in api

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiCrud.Context;
+using WebApiCrud.Entities;
+
+namespace WebApiCrud.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ContatoController : ControllerBase
+{
+    private readonly AgendaContext _context;
+
+    public ContatoController(AgendaContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpPost]
+    public IActionResult Create(Contato contato)
+    {
+        _context.Add(contato);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(ObterPorId), new { id = contato.Id }, contato);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult ObterPorId(int id)
+    {
+        var contato = _context.Contatos.Find(id);
+        if (contato == null)
+            return NotFound();
+        
+        return Ok(contato);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Atualizar(int id, Contato contato)
+    {
+        var contatoBanco = _context.Contatos.Find(id);
+        if (contatoBanco == null)
+            return NotFound();
+
+        contatoBanco.Nome = contato.Nome;
+        contatoBanco.Telefone = contato.Telefone;
+        contatoBanco.Ativo = contato.Ativo;
+
+        _context.Contatos.Update(contatoBanco);
+        _context.SaveChanges();
+
+        return Ok(contatoBanco);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Deletar(int id)
+    {
+        var contatoBanco = _context.Contatos.Find(id);
+        if (contatoBanco == null)
+            return NotFound();
+
+        _context.Contatos.Remove(contatoBanco);
+        _context.SaveChanges();
+        
+        return NoContent();
+    }
+}
